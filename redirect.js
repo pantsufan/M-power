6ece7f79f306600576c002e084d88d1e3fa50c5e
Joel Rodrigues <45329484+pantsufan@users.noreply.github.com>
2019-09-17 13:22:34 +0000

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,16 @@
+function rm1()
+{
+    window.location.assign("posts/news1.html")
+}
+function rm2()
+{
+    window.location.assign("posts/news2.html")
+}
+function rm3()
+{
+    window.location.assign("posts/news3.html")
+}
+function rm4()
+{
+    window.location.assign("posts/news4.html")
+}
